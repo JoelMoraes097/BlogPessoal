057bf7fcb03782960f68f55186a8d35de6f6cfe1
JoelMoraes <juniormoraes00@live.com>
2021-12-02 20:27:56 -0300

    novos metodos

--- src/main/java/org/Generation/BlogPessoal/Controller/PostagemController.java
+++ src/main/java/org/Generation/BlogPessoal/Controller/PostagemController.java
@@ -27,34 +27,31 @@ public class PostagemController {
 
 	@GetMapping
 	public ResponseEntity<List<Postagem>> GetAll() {
-		 return ResponseEntity.ok(repository.findAll());
-	} 
-    
-	@GetMapping ("/{id}")
-    public ResponseEntity<Postagem> GetById(@PathVariable Long id) {
-    	return repository.findById(id)
-         .map(resp -> ResponseEntity.ok(resp))
-         .orElse(ResponseEntity.notFound().build());
-   	}
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> GetById(@PathVariable Long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
 
 	@GetMapping("/titulo/{titulo}")
-	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
+	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
+	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
 	}
-	
+
 	@PutMapping
-	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem){
+	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem) {
 		return ResponseEntity.status(201).body(repository.save(postagem));
 	}
 
-	
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable long id) {
 		repository.deleteById(id);
-	}	
+	}
 }
--- src/main/java/org/Generation/BlogPessoal/repository/PostagemRepository.java
+++ src/main/java/org/Generation/BlogPessoal/repository/PostagemRepository.java
@@ -11,7 +11,4 @@ public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
 	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 	
-//	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo); 	
-	
-
 }
